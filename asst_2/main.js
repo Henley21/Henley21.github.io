8dc9d8c3ecae259df3cfb6a06bfd178b00c85b0c
henley21 <hkhranov@terpmail.umd.edu>
2020-04-02 23:45:31 -0400

    Revert "Merge branch 'master' of https://github.com/Henley21/Henley21.github.io"
    This reverts commit 2b72c24b6e8b717e64f81578343e56bb915a77b5, reversing
    changes made to b31eb852004965b5aceae5de5aa5895b273b1b96.

--- asst_2/main.js
+++ asst_2/main.js
@@ -14,14 +14,14 @@ for (let i = 1; i < 6; i++) {
     newImage.onclick = function() {
         displayedImage.getAttribute('src')
         displayedImage.setAttribute('src', newImage.src)
-    }   //https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers/onclick
+    }
     
 
 }
 
 /* Wiring up the Darken/Lighten button */
-//let but = document.getElementById('onClick');
-//btn.getAttribute('onClick', xxx);
-//btn.textContent = xxx;
-//overlay.style.backgroundColor = xxx;
-//https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Image_gallery 
+let but = document.getElementById('onClick');
+btn.getAttribute('onClick', xxx);
+btn.textContent = xxx;
+overlay.style.backgroundColor = xxx;
+/*https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Image_gallery */
